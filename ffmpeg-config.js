c318e7a12a71889c454cadea5c9d43e5be8805f4
bruyen72 <brunoruthes92@gmail.com>
2025-06-09 08:32:38 -0400

    Add FFmpeg 7.1.1 configuration with download instructions

---
+++ ffmpeg-config.js
@@ -0,0 +1,8 @@
+const ffmpegConfig = { 
+  version: "7.1.1", 
+  downloadUrl: "https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-win64-gpl.zip", 
+  path: "./ffmpeg-7.1.1/bin/ffmpeg.exe", 
+  installRequired: true, 
+  note: "Download and extract FFmpeg to ffmpeg-7.1.1 folder" 
+}; 
+module.exports = ffmpegConfig; 
